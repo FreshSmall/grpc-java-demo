df65b8d9069740b7cd47a81bdc5a8d391a9117e1
yinchao <ycsuper2819@gmail.com>
2022-08-17 14:52:08 +0800

    修改grpc-java 版本为V1.5.0

--- grpc-java-server/src/main/java/GreeterGrpcImpl.java
+++ grpc-java-server/src/main/java/GreeterGrpcImpl.java
@@ -8,6 +8,7 @@ public class GreeterGrpcImpl extends GreeterGrpc.GreeterImplBase {
     @Override
     public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
         String name = request.getName();
+        System.out.println("收到请求信息:"+name);
         HelloReply.Builder builder = HelloReply.newBuilder();
         builder.setMessage(String.format("hello 方法处理结果%s", name));
         responseObserver.onNext(builder.build());
